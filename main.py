89ba1ac0274df3887df41e2e9c78a1858a434417
Vladimir <vladimir2balakin@yandex.ru>
2020-12-09 17:07:08 +0300

    Formatted

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-#from time import sleep
+# from time import sleep
 from random import randint
 
 import telebot
 
-from base import addUserIfItIsNotInStatistic, increaseUserStatistic, winner, returnStatistic, returnGameArray, readFromFile, writeToFile
+from base import addUserIfItIsNotInStatistic, increaseUserStatistic, winner, returnStatistic, returnGameArray, \
+    readFromFile, writeToFile
 from config import botToken
 
 bot = telebot.TeleBot(botToken)
 
 readFromFile()
 
+
 def returnNameOrId(msg):
-    if not((msg.chat.first_name is None) or (msg.chat.last_name is None)):
+    if not ((msg.chat.first_name is None) or (msg.chat.last_name is None)):
         return msg.chat.first_name + '_' + msg.chat.last_name
     return msg.chat.id
 
-@bot.message_handler(commands = ['start'])
+
+@bot.message_handler(commands=['start'])
 def starting(msg):
-    questions = [["Ваш пол", "Мужской", "Женский"], ["Ваш возраст", "Меньше или равен 18", "Больше 18 и меньше 25", "Больше или равен 25"], ["Вы - нервный человек? ", "Да", "Скорее да, чем нет", "Скорее нет, чем да", "Нет"], ["Что бы вы делали, если бы на вас хотел напасть некий человек(у вас с ним равные физические показатели)", "Попробовал бы договориться", "Убежал", "Встал бы в ступор", "Защитился", "Атаковал"], ["Вы склонны к депрессии?", "Да", "Скорее да, чем нет", "Скорее нет, чем да", "Нет"], ["Вы - апатичный человек?", "Да", "Скорее да, чем нет", "Скорее нет, чем да", "Нет"], ["Как часто вы играете в камень ножницы бумага?", "Больше 1 раза в день", "1 раз в день", "1 раз в неделю", "1 раз в месяц", "1 раз в год", "Меньше 1 раза в год", "Никогда"]]
+    questions = [["Ваш пол", "Мужской", "Женский"],
+                 ["Ваш возраст", "Меньше или равен 18", "Больше 18 и меньше 25", "Больше или равен 25"],
+                 ["Вы - нервный человек? ", "Да", "Скорее да, чем нет", "Скорее нет, чем да", "Нет"], [
+                     "Что бы вы делали, если бы на вас хотел напасть некий человек(у вас с ним равные физические "
+                     "показатели)",
+                     "Попробовал бы договориться", "Убежал", "Встал бы в ступор", "Защитился", "Атаковал"],
+                 ["Вы склонны к депрессии?", "Да", "Скорее да, чем нет", "Скорее нет, чем да", "Нет"],
+                 ["Вы - апатичный человек?", "Да", "Скорее да, чем нет", "Скорее нет, чем да", "Нет"],
+                 ["Как часто вы играете в камень ножницы бумага?", "Больше 1 раза в день", "1 раз в день",
+                  "1 раз в неделю", "1 раз в месяц", "1 раз в год", "Меньше 1 раза в год", "Никогда"]]
     for i in questions:
-        bot.send_poll(msg.chat.id, i[0], i[1::], is_anonymous = False)
+        bot.send_poll(msg.chat.id, i[0], i[1::], is_anonymous=False)
     ans = returnGameArray()
     bot.send_message(msg.chat.id, ans)
 
-@bot.message_handler(commands = ['help'])
-def help(msg):
+
+@bot.message_handler(commands=['help'])
+def helping(msg):
     ans = returnGameArray()
     bot.send_message(msg.chat.id, ans)
 
-@bot.message_handler(commands = ['statistic'])
+
+@bot.message_handler(commands=['statistic'])
 def coll(msg):
     bot.send_message(msg.chat.id, returnStatistic())
 
-@bot.message_handler(commands = ['Paper', 'Stone', 'Scissors'])
+
+@bot.message_handler(commands=['Paper', 'Stone', 'Scissors'])
 def answer(msg):
     if msg.text[-24::] == "@CollectingStatisticsBot":
         text = msg.text[:-24:]
@@ -51,7 +66,10 @@ def answer(msg):
     increaseUserStatistic(id, text, win[0])
     writeToFile()
 
-@bot.message_handler(content_types = ['text'])
+
+@bot.message_handler(content_types=['text'])
 def answer_(msg):
     pass
+
+
 bot.polling()
